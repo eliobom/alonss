6ee29543821a0a5e578478eeb7a62216c1c11447
User <user@example.com>
2025-11-18 20:02:23 -0500

    Hardcodear variables de Supabase para despliegue

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
+const supabaseUrl = 'https://skcfvsarhmkuxealccxa.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrY2Z2c2FyaG1rdXhlYWxjY3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1MTE1MTAsImV4cCI6MjA3OTA4NzUxMH0.G6ezFcGPXG45j2iweI_G-9XwwJQgHJko2LHtD7KotT4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
